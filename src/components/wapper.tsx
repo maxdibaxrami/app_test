2c70d2c4f6bdb29984a34e19fcc6632c50b646d7
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2025-04-07 21:20:25 +0300

    m

--- src/components/wapper.tsx
+++ src/components/wapper.tsx
@@ -11,7 +11,7 @@ const MobileApp = ({ children }) => {
       className="fixed w-screen  left-0 top-0 right-0 bottom-0 h-screen "
       id="wrap"
       style={{
-        overflowY: searchParams.get('page') === "explore" ? "hidden" :"scroll",
+        overflowY: searchParams.get('page') === "explore" || searchParams.get('page') === "RandomChat"  ? "hidden" :"scroll",
         overflowX:"hidden"
       }}
     >
