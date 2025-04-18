85e162412d7769fe35c96e6b35f5be4673ad23aa
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2025-04-18 11:17:12 +0300

    m

--- src/components/wapper.tsx
+++ src/components/wapper.tsx
@@ -1,17 +1,18 @@
 import { publicUrl } from "@/helpers/publicUrl";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
-import { useSearchParams } from "react-router-dom";
+import { useLocation, useSearchParams } from "react-router-dom";
 
 const MobileApp = ({ children }) => {
   const [searchParams] = useSearchParams();
-
+  const location = useLocation();
+  
   
   return (
     <div
       className="fixed w-screen  left-0 top-0 right-0 bottom-0 h-screen "
       id="wrap"
       style={{
-        overflowY: searchParams.get('page') === "explore"  ? "hidden" :"scroll",
+        overflowY: searchParams.get('page') === "explore" || searchParams.get('page') === "RandomChat" || location.pathname.includes('chat-detail') ? "hidden" :"scroll",
         overflowX:"hidden"
       }}
     >
