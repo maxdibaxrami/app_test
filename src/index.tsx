9ee96df1cd53ec711104191ae91fc2e9641dd5ba
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2025-05-20 20:01:30 +0300

    m

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import { Provider } from 'react-redux';
 import { store } from './store.ts';
 
 telegramAnalytics.init({
-  token: 'eyJhcHBfbmFtZSI6ImZhc2VfbWF0Y2hfYm90IiwiYXBwX3VybCI6Imh0dHBzOi8vdC5tZS9mYXNlX21hdGNoX2JvdCIsImFwcF9kb21haW4iOiJodHRwczovL2NvcHljaGljLnJ1LyJ9!OD0pBnaM8z/WpinwnaEG6GeKv61yRn3x6AI0XQ4hEqc=', // SDK Auth token received via @DataChief_bot
+  token: 'eyJhcHBfbmFtZSI6ImZhc2VfbWF0Y2hfYm90IiwiYXBwX3VybCI6Imh0dHBzOi8vdC5tZS9mYXNlX21hdGNoX2JvdCIsImFwcF9kb21haW4iOiJodHRwczovL2FwcC10ZXN0LW9tZWdhLnZlcmNlbC5hcHAvIn0=!QC07rQlVq8NcBKPJeC6uM/OOBcAoZd30o09AiD7Bixg=', // SDK Auth token received via @DataChief_bot
   appName: 'fase_match_bot', // The analytics identifier you entered in @DataChief_bot
 });
 
